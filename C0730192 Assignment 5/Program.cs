051bf5e54327506aef562b5657a7918d5ce6d550
visualstudio <visualstudio@122-PC40>
2019-04-12 18:41:40 -0400

    Gematria

--- C0730192 Assignment 5/Program.cs
+++ C0730192 Assignment 5/Program.cs
@@ -4,32 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C0730192_Assignment_5
+namespace Gematri
 {
-    class Program2
+    class Program
     {
-        
-        string[] Letters = new string[26] { "a", "b", "c", "d", "e", "f","g","h","i", "j","k","l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "V", "W","X","y","z" };
+
+        string[] Letters = new string[26] {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "1",
+"m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
         public static void Main()
         {
-            Program2 orange=new Program2();
+
+            Program orange = new Program();
             Console.WriteLine(orange.Gematria("universe"));
+            Console.ReadLine();
         }
+
         public int Gematria(string word)
         {
+
             int GematriaValue = 0;
             for (int i = 0; i < word.Length; i++)
             { GematriaValue += LetterValue(word[i].ToString()); }
             return GematriaValue;
         }
-        public int LetterValue(string letter) { 
-        int x = 0;
-        while(Letters[x]!=letter)
-            {
-                if(Letters[x++]== letter)return x;
-            }
-            return-1;
-        }
 
+        public int LetterValue(string letter)
+        {
+            int x = 0;
+            while (Letters[x] != letter)
+            {
+                if (Letters[x++] == letter) return -1;
+            }
+            return x + 1;
+        }
     }
 }
